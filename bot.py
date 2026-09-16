914301dd39d9af8e507bf7b72b90a23823edf6b8
bekkitachi-cyber <bekkitachi@gmail.com>
2026-09-16 12:51:08 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -222,8 +222,7 @@ async def cmd_start(message: Message, bot: Bot):
             "Kino kodini yuboring - shu kodga mos kino keladi.\n"
             "Yoki kino nomini yozing - mos kinolar ro'yxati chiqadi."
         )
-    except Exception as e:
-        pass)
+    except Exception as e:pass)
 @user_router.message(F.text.regexp(r"^\d+$"))
 async def get_by_code(message: Message):
     code = int(message.text.strip())
